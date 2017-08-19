f440ddf91d3185bc206f86bfcffca80b391747c5
Sasha Tatar <sasa.tatar@gmail.com>
2017-08-19 19:30:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// https://en.wikipedia.org/wiki/Round-robin_tournament
+function bergerTable(n) {
+    const numberOfRounds = n % 2 === 0 ? n-1 : n;
+    const gamesPerRound = Math.floor(n/2);
+    let count = 0;
+    let rounds = Array.from({length: numberOfRounds}).map((_, i) => {
+        return Array.from({length: gamesPerRound}).map((_, k) => {
+            return {
+                round: i+1,
+                game: k+1,
+                teamA: count++ % n + 1,
+                teamB: null
+            }
+        })
+    });
+    let index = 0;
+    let teams = Array.from({length: n}).map((_, i) => i+1).reverse();
+    return rounds.map(round => {
+        return round.map(game => {
+            let teamB = teams[index++];
+            if (index > n-1) {
+                index = 0;
+                teams.push(teams.shift());
+            }
+            game.teamB = teamB !== game.teamA ? teamB : teams[index++];
+            return game;
+        })
+    })
+}
+
+console.log(bergerTable(7));
+console.log('\n');
+console.log(bergerTable(9));
